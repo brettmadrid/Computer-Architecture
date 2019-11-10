222090a5b87f9342df63e6f65a0946600c512f44
brettmadrid <brettmadrid9@gmail.com>
2019-11-10 15:32:00 -0800

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,19 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+POP = 0b01000110 
+PUSH = 0b01000101
+RET = 0b00010001
+CALL = 0b01010000
+JMP = 0b01010100
+JEQ = 0b01010101 
+JNE = 0b01010110 
+CMP = 0b10100111
+
 class CPU:
     """Main CPU class."""
 
@@ -9,30 +22,28 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
-        self.pc = 0
-        self.ir = None
-        self.fl = 0
+        self.pc = 0               
+        self.fl = 6               
+        self.SP = 7          
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        try:
+            with open(program) as file:
+                for line in file:
+                    comment_split = line.split('#')
+                    maybe_command = comment_split[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    if maybe_command == '':
+                        continue
+                    self.ram[address] = int(maybe_command, 2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    address +=1
+        except FileNotFoundError:
+            print('File does not exist')
 
 
     def alu(self, op, reg_a, reg_b):
@@ -40,7 +51,19 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        if op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        if op == "CMP":
+            result = 0
+            if self.reg[reg_a] == self.reg[reg_b]:
+                result = 1
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                result = 2
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                result = 4
+
+            self.reg[self.fl] = result
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -52,8 +75,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -69,19 +90,85 @@ class CPU:
         running = True
 
         while running:
-            memory_address = self.pc
-            self.ir = memory_address
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            # depending on the opcode 
-            # fill this in
 
-            self.pc += 3
+            ir = self.ram[self.pc]   
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
 
+            if ir == LDI:
+                print(f'LDI ir is: {ir}')
+                self.reg[operand_a] = operand_b
+                print(f'reg is now: {self.reg[operand_a]}')
+                self.pc += 3
 
+            elif ir == PRN:
+                print(f'PRN ir is: {ir}')
+                value = self.reg[operand_a]
+                print(value)
+                self.pc += 2
+
+            elif ir == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+                
+            elif ir == CMP:
+                self.alu("CMP", operand_a, operand_b)
+                self.pc += 3
+
+            elif ir == PUSH:
+                self.reg[7] -= 1
+
+                reg_address = self.ram[self.pc + 1]
+                value = self.reg[reg_address]
+
+                self.SP = self.reg[7]
+                self.ram[self.SP] = value
+
+                self.pc += 2
+
+            elif ir == POP:
+                self.SP = self.reg[7]
+                value = self.ram[self.SP]
+                target_reg_address = self.ram[self.pc + 1]
+                self.reg[target_reg_address] = value
+
+                self.reg[7] += 1
+
+                self.pc += 2
+            
+            elif ir == CALL:
+                self.reg[self.SP] -= 1
+                self.ram[self.reg[self.SP]] = self.pc + 2
+                self.pc = self.reg[operand_a]
+
+            elif ir == RET:
+                value = self.ram[self.reg[self.SP]]
+                self.pc = value
+
+                self.reg[self.SP] += 1
+            
+            elif ir == JMP:
+                self.pc = self.reg[operand_a]
+
+            elif ir == JEQ:
+                if self.reg[self.fl] == 1:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+
+            elif ir == JNE:
+                if self.reg[self.fl] != 1:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+
+            elif ir == HLT:
+                print(f'Entered HLT. CPU ending...')
+                running = False
 
     def ram_read(self, MAR):
         return self.ram[MAR]
 
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
+    
